142702eaa69374998bfa18fc9e387ee1d12ade27
thanusri0208 <thanusrigudi@gmail.com>
2024-06-29 19:59:28 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,76 @@
+require 'src/Dependencies'
+
+function love.load()
+    math.randomseed(os.time())
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Angry 50')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true
+    })
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
+    }
+    gStateMachine:change('start')
+
+    gSounds['music']:setLooping(true)
+    gSounds['music']:play()
+
+    love.keyboard.keysPressed = {}
+    love.mouse.keysPressed = {}
+    love.mouse.keysReleased = {}
+
+    paused = false
+end
+
+function push.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'p' then
+        paused = not paused
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.mousepressed(x, y, key)
+    love.mouse.keysPressed[key] = true
+end
+
+function love.mousereleased(x, y, key)
+    love.mouse.keysReleased[key] = true 
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.mouse.wasPressed(key)
+    return love.mouse.keysPressed[key]
+end
+
+function love.mouse.wasReleased(key)
+    return love.mouse.keysReleased[key]
+end
+
+function love.update(dt)
+    if not paused then
+        gStateMachine:update(dt)
+
+        love.keyboard.keysPressed = {}
+        love.mouse.keysPressed = {}
+        love.mouse.keysReleased = {}
+    end
+end
+
+function love.draw()
+    push:start()
+    gStateMachine:render()
+    push:finish()
+end
